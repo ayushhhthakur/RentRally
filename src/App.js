bcfb61482d0841e6a9ec081b6dbce2b69810d2d7
Ayush Thakur <ayushthakur1412@gmail.com>
2023-09-04 15:02:07 +0530

    Added LearnMore Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Contact from "./Pages/Contact";
 import Login from "./Pages/Login";
 import Register from "./Pages/register";
+import LearnMore from "./Pages/LearnMore";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="contact" element={<Contact />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="LearnMore" element={<LearnMore />} />
       </Routes>
     </>
   );
