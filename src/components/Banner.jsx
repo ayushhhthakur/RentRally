e813f9c62f3fcb07c80fbaaa9a5eb8ca8ae670a0
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-10-18 12:40:28 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,17 @@
-function Banner() {
+import React from 'react';
+
+const Banner = () => {
   return (
-    <>
-      <section className="banner-section">
-        <div className="container">
-          <div className="banner-content">
-            <div className="banner-content__text">
-              <h2>Save big with our cheap car rental!</h2>
-              <p>
-                Top Airports. Local Suppliers. <span>24/7</span> Support.
-              </p>
-            </div>
+    <section className="banner-section">
+      <div className="container">
+        <div className="banner-content">
+          <div className="banner-content__text">
+            <h2>Discover Big Savings on Our Affordable Car Rentals!</h2>
+            <p>Choose from a Variety of Cars. Local Suppliers. <span>24/7</span> Support.</p>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
 
