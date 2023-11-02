78d853bc4adb1e490d1c5e783cfe52afcda4c5dd
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-11-02 23:46:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { createUser, findUserByEmail } = require('./db'); // Update the path accordingly
+const { createUser, findUserByEmail } = require('./db');
 
 const app = express();
 const PORT = 3001;
