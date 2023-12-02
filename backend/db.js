25158b4b9b22ab5b1645068e9d42328474347e44
Ayush Thakur <100257355+ayushhhthakur@users.noreply.github.com>
2023-12-02 13:58:18 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,43 +1,41 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// <<<<<<< backend
-// mongoose.connect('mongodb://localhost:27017/rentrally.users', {
-// =======
-// // Connect to MongoDB
-// mongoose.connect('mongodb://localhost:27017/rentrally.users', { // use .env for mongoDB credentials
-// >>>>>>> main
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/rentrally.users', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// const userSchema = new mongoose.Schema({
-//   firstName: String,
-//   lastName: String,
-//   email: String,
-//   password: String,
-// });
+// Create a user schema
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+});
 
-// const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-// module.exports = {
-//   User,
-//   createUser: async (userData) => {
-//     try {
-//       const newUser = new User(userData);
-//       await newUser.save();
-//       console.log('User created successfully');
-//       return newUser;
-//     } catch (error) {
-//       console.error('Error creating user:', error);
-//       throw error;
-//     }
-//   },
-//   findUserByEmail: async (email) => {
-//     try {
-//       return await User.findOne({ email });
-//     } catch (error) {
-//       console.error('Error finding user by email:', error);
-//       throw error;
-//     }
-//   },
-// };
+// Export the model and functions for database operations
+module.exports = {
+  User,
+  createUser: async (userData) => {
+    try {
+      const newUser = new User(userData);
+      await newUser.save();
+      console.log('User created successfully');
+      return newUser;
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw error;
+    }
+  },
+  findUserByEmail: async (email) => {
+    try {
+      return await User.findOne({ email });
+    } catch (error) {
+      console.error('Error finding user by email:', error);
+      throw error;
+    }
+  },
+};
